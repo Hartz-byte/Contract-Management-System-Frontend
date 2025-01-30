da0a72ed9a460b65fae22f264c1c96b9c7462af4
Harsh Gupta <harshmail281199@gmail.com>
2025-01-30 15:59:39 +0530

    vite.config.js and vercel.json files updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
@@ -18,5 +16,11 @@ export default defineConfig({
         ws: true,
       },
     },
+  },
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
   },
 });
