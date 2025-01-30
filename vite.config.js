109c24540c247d201f699110d5b7e831051bfbe1
Harsh Gupta <harshmail281199@gmail.com>
2025-01-30 16:58:33 +0530

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
+import process from "node:process";
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        // target: "http://localhost:5000",
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-      },
-      "/socket.io": {
-        // target: "http://localhost:5000",
-        target: import.meta.env.VITE_WS_URL,
-        changeOrigin: true,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
       },
     },
-  },
-  base: "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-  },
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        "/socket.io": {
+          target: env.VITE_WS_URL,
+          changeOrigin: true,
+          ws: true,
+        },
+      },
+    },
+    base: "/",
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      sourcemap: false,
+    },
+  };
 });
